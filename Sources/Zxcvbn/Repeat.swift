eb6caa308182fac6e970fa87fb758e6a595978ff
Todd Heasley <toddheasley@me.com>
2020-05-10 22:05:56 -0400

    Add repeated character matching

---
+++ Sources/Zxcvbn/Repeat.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+struct Repeat {
+    
+}
+
+extension Repeat: Matching {
+    
+    // MARK: Matching
+    func matches(_ string: String) -> [Match] {
+        let components: [String] = string.map { character in
+            return "\(character)"
+        }
+        var matches: [RepeatMatch] = []
+        var i: Int = 0
+        while i < components.count {
+            var j: Int = i + 1
+            while true {
+                let previousComponent: String = components[j - 1]
+                let currentComponent: String = j < components.count ? components[j] : ""
+                if previousComponent == currentComponent {
+                    j += 1
+                } else {
+                    if j - i > 2 {
+                        let range: ClosedRange<Int> = i...(j - 1)
+                        matches.append(RepeatMatch(repeated: components[i], range: range, token: components[range].joined()))
+                    }
+                    break
+                }
+            }
+            i = j
+        }
+        return matches
+    }
+}
+
+struct RepeatMatch: Match {
+    let repeated: String
+    
+    // MARK: Match
+    let range: ClosedRange<Int>
+    let token: String
+    let pattern: String = "repeat"
+}
---
+++ Tests/ZxcvbnTests/RepeatTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import Zxcvbn
+
+final class RepeatTests: XCTestCase {
+    
+}
+
+extension RepeatTests {
+    
+    // MARK: Matching
+    func testStringMatches() {
+        XCTAssertEqual(Repeat().matches("zxcvbn").count, 0)
+        XCTAssertEqual(Repeat().matches("qwER43@!").count, 0)
+        XCTAssertEqual(Repeat().matches("D0g..................").count, 1)
+        XCTAssertEqual(Repeat().matches("aaaEEiiIooooooUU").count, 2)
+        XCTAssertEqual(Repeat().matches("aaaEEiiiiIIIooooooUU").count, 4)
+        XCTAssertEqual(Repeat().matches("ryanhunter2000").count, 1)
+        XCTAssertEqual(Repeat().matches("rianhunter2000").count, 1)
+        XCTAssertEqual(Repeat().matches("").count, 0)
+    }
+}
